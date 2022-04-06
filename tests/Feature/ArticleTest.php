c9cd53e0c992eb0bccb55efdf6e338cf926b1961
uglo1 <yujiro.takahashi0912@gmail.com>
2022-04-06 21:54:13 +0900

    ✅ isLikedBy Another and TheUser

--- tests/Feature/ArticleTest.php
+++ tests/Feature/ArticleTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Article;
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -11,11 +12,37 @@ class ArticleTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function testIsLikedByNull() {
+    public function testIsLikedByNull() 
+    {
         $article = factory(Article::class)->create();
         
         $result = $article->isLikedBy(null);
     
+        $this->assertFalse($result);
+    }
+
+    public function testIsLikedByTheUser()
+    {
+        $article = factory(Article::class)->create();
+        $user = factory(User::class)->create();
+        $article->likes()->attach($user);
+
+        $result = $article->isLikedBy($user);
+
+        $this->assertTrue($result);
+    }
+
+    public function testIsLikedByAnother()
+    {
+        $article = factory(Article::class)->create();
+        $user = factory(User::class)->create();
+        $another = factory(User::class)->create();
+        // 自分ではない他人が記事にいいねをする
+        $article->likes()->attach($another);
+
+        // $userがこの記事をいいねしているかの結果
+        $result = $article->isLikedBy($user);
+
         $this->assertFalse($result);
     }
 }
